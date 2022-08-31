bc3fe757f0d4931ca61892234feff92f22965ca4
castrojo012495 <100735180+castrojo012495@users.noreply.github.com>
2022-08-31 14:58:06 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: darthcastle
+ *
+ * Created on August 31, 2022, 2:53 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+        cout<< "Hello World!!";
+    return 0;
+}
+
